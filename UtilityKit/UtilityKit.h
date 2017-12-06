432d8db009f715b07a5707d0c115b86b83ccd5d5
Kacper Raczy <gfw.kra@gmail.com>
2017-12-07 00:31:03 +0100

    Added UIKit and Foundation Extensions.

---
+++ UtilityKit/UtilityKit.h
@@ -0,0 +1,19 @@
+//
+//  UtilityKit.h
+//  UtilityKit
+//
+//  Created by Kacper Raczy on 07.12.2017.
+//  Copyright © 2017 Kacper Raczy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtilityKit.
+FOUNDATION_EXPORT double UtilityKitVersionNumber;
+
+//! Project version string for UtilityKit.
+FOUNDATION_EXPORT const unsigned char UtilityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilityKit/PublicHeader.h>
+
+
